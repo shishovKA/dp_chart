9cbfbc2050310f2e7a54119de3ee07be311a6755
Konstantin Shishov <shishov.k.a@yandex.ru>
2020-10-23 18:48:29 +0300

    normal config version

--- src/index.ts
+++ src/index.ts
@@ -18,28 +18,9 @@ import { cbh1 } from "./data/cbh1"
 import { cbh5 } from "./data/cbh5"
 import { xLabels } from "./data/xLabels"
 
-//работа с виджетом для отображения индекса данных
-const indexWidget = document.getElementById('chart_w_ind');
-
-function conncetIndexWidget(index: number) {
-  indexWidget?.textContent = index.toString();
-}
-
-//работа с виджетом для отображения данных красного графика
-const redWidget = document.getElementById('chart_w_data_red');
-
-function conncetRedWidget(index: number) {
-  redWidget?.textContent = cbh1[index].toFixed(1);
-}
-
-//работа с виджетом для отображения данных синего графика
-const blueWidget = document.getElementById('chart_w_data_blue');
-
-function conncetBlueWidget(index: number) {
-  blueWidget?.textContent = cbh5[index].toFixed(1);
-}
-
-
+const zeroSeries = cbh1.map(() => {
+  return 0;
+})
 
 let chart: Chart;
 
@@ -54,101 +35,33 @@ WebFont.load({
     chart.tooltipsDataIndexUpdated.add(conncetIndexWidget);
     chart.tooltipsDataIndexUpdated.add(conncetRedWidget);
     chart.tooltipsDataIndexUpdated.add(conncetBlueWidget);
+    chart.tooltipsDraw(true);
   },
 
 });
 
 
+//настройка виджетов для отображения данных Тултипов
+const indexWidget = document.getElementById('chart_w_ind');
 
-/*
-const panel = new Panel(document.querySelector('.panel'), 'X axis (0 - 974)', 3,'Min', 0, 'Max', 900, 'duration', 1000);
+function conncetIndexWidget(index: number) {
+  indexWidget?.textContent = index.toString();
+}
 
-panel.submitBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  const values = panel.values;
+const redWidget = document.getElementById('chart_w_data_red');
 
+function conncetRedWidget(index: number) {
+  redWidget?.textContent = cbh1[index].toFixed(1);
+}
 
-  if (values[2] != 0) {
-      chart.xAxis.setMinMax([values[0],values[1]], values[2]);
-      chart.yAxis.setMinMax(chart.data.findExtremes('ind', values[0], values[1]), values[2]);
-    } else {
-      chart.xAxis.setMinMax([values[0],values[1]]);
-      chart.yAxis.setMinMax(chart.data.findExtremes('ind', values[0], values[1]));
-      }
+const blueWidget = document.getElementById('chart_w_data_blue');
 
-});
-
-const panelStep = new Panel(document.querySelector('.panel'), 'Y axis ticks', 2,'Step', 100, 'duration', 1000);
-
-panelStep.submitBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  const values = panelStep.values;
-  chart.yAxis.ticks.setOptions('fixedStep', values[0]);
-});
+function conncetBlueWidget(index: number) {
+  blueWidget?.textContent = cbh5[index].toFixed(1);
+}
 
 
-const lastLb = xLabels[xLabels.length-1]
-
-const btnDuration = 1000;
-//кнопка 1st Y
-const FirstYBtn = new Btn(document.querySelector('.panel'),'1st Y','#e5e6e1');
-
-FirstYBtn.element.addEventListener("click", (event) => {
-    const minDate = dateParser(xLabels[0]);
-    const maxDate = minDate.setFullYear(minDate.getFullYear() + 1);
-    const max = findDateInd(maxDate);
-    const min = 0;
-    chart.xAxis.setMinMax([min,max], btnDuration);
-    chart.yAxis.setMinMax(chart.data.findExtremes('ind', min, max), btnDuration);
-  })
-
-
-//кнопка 6M
-const SixMBtn = new Btn(document.querySelector('.panel'),'6M','#e5e6e1');
-
-SixMBtn.element.addEventListener("click", (event) => {
-    const maxDate = dateParser(lastLb);
-    const minDate = maxDate.setMonth(maxDate.getMonth() - 6);
-    const max = xLabels.length-1;
-    const min = findDateInd(minDate);
-    chart.xAxis.setMinMax([min,max], btnDuration);
-    chart.yAxis.setMinMax(chart.data.findExtremes('ind', min, max), btnDuration);
-  })
-
-//кнопка 1Y
-const OneYBtn = new Btn(document.querySelector('.panel'),'1Y','#e5e6e1');
-
-OneYBtn.element.addEventListener("click", (event) => {
-    const maxDate = dateParser(lastLb);
-    const minDate = maxDate.setFullYear(maxDate.getFullYear() - 1);
-    const max = xLabels.length-1;
-    const min = findDateInd(minDate);
-    chart.xAxis.setMinMax([min,max], btnDuration);
-    chart.yAxis.setMinMax(chart.data.findExtremes('ind', min, max), btnDuration);
-  })
-
-//кнопка 2Y
-const TwoYBtn = new Btn(document.querySelector('.panel'),'2Y','#e5e6e1');
-
-TwoYBtn.element.addEventListener("click", (event) => {
-    const maxDate = dateParser(lastLb);
-    const minDate = maxDate.setFullYear(maxDate.getFullYear() - 2);
-    const max = xLabels.length-1;
-    const min = findDateInd(minDate);
-    chart.xAxis.setMinMax([min,max], btnDuration);
-    chart.yAxis.setMinMax(chart.data.findExtremes('ind', min, max), btnDuration);
-  })
-
-//кнопка Max
-const MaxBtn = new Btn(document.querySelector('.panel'),'MAX','#e5e6e1');
-
-MaxBtn.element.addEventListener("click", (event) => {
-    const max = xLabels.length-1;
-    const min = 0;
-    chart.xAxis.setMinMax([min,max], btnDuration);
-    chart.yAxis.setMinMax(chart.data.findExtremes('ind', min, max), btnDuration);
-  })
-
+//настройка кнопок для управлением диапазоном оси дат
 
 //вспомогательные функции для работы с датами для определения области построения графика
 function dateParser(myDate: string) {
@@ -168,7 +81,207 @@ function findDateInd(date: Date) {
   return ind;
 }
 
-*/
+
+// подготавливаем данные как на сайте CyberHedge
+function reorganizeChart(cbh5, cbh1, min, max) {
+  let data = prepareDataforCbh(cbh5, cbh1, min);
+  let {
+    serie5star,
+    area5starTop,
+    area5starBottom,
+    serie1star,
+    area1starTop,
+    area1starBottom
+  } = data;
+
+  chart.data.findSeriesById('cyberHedge5_area')?.replaceSeriesData([area5starTop, area5starBottom]);
+  chart.data.findSeriesById('cyberHedge1_area')?.replaceSeriesData([area1starTop, area1starBottom]);
+  chart.data.findSeriesById('cyberHedge5_line')?.replaceSeriesData([serie5star]);
+  chart.data.findSeriesById('cyberHedge1_line')?.replaceSeriesData([serie1star]);
+
+  chart.xAxis.setMinMax([min,max], false);
+  chart.yAxis.setMinMax(chart.data.findExtremes('ind', min, max), true);
+}
+
+//последняя дата
+const lastLb = xLabels[xLabels.length-1];
+
+//кнопка 6M
+const SixMBtn = document.getElementById('6M');
+
+SixMBtn.addEventListener("click", (event) => {
+    const maxDate = dateParser(lastLb);
+    const minDate = maxDate.setMonth(maxDate.getMonth() - 6);
+    const max = xLabels.length-1;
+    const min = findDateInd(minDate);
+    reorganizeChart(cbh5, cbh1, min, max);
+  })
+
+//кнопка 1Y
+const OneYBtn = document.getElementById('1Y');
+
+OneYBtn.addEventListener("click", (event) => {
+    const maxDate = dateParser(lastLb);
+    const minDate = maxDate.setFullYear(maxDate.getFullYear() - 1);
+    const max = xLabels.length-1;
+    const min = findDateInd(minDate);
+    reorganizeChart(cbh5, cbh1, min, max);
+  })
+
+//кнопка 2Y
+const TwoYBtn = document.getElementById('2Y');
+
+TwoYBtn.addEventListener("click", (event) => {
+  const maxDate = dateParser(lastLb);
+  const minDate = maxDate.setFullYear(maxDate.getFullYear() - 2);
+  const max = xLabels.length - 1;
+  const min = findDateInd(minDate);
+  reorganizeChart(cbh5, cbh1, min, max);
+})
+
+//кнопка Max
+const MaxBtn = document.getElementById('MAX');
+
+MaxBtn.addEventListener("click", (event) => {
+  const max = xLabels.length - 1;
+  const min = 0;
+  reorganizeChart(cbh5, cbh1, min, max);
+})
+
+
+
+function calculateDeviations(rowData: number[], fromIndex: number) {
+  let chartDataVariation = [];
+  let zeroPoint = rowData[fromIndex];
+
+  chartDataVariation = [];
+
+  for (let j = 0, m = rowData.length; j < m; j++) {
+    chartDataVariation.push(100 * (rowData[j] - zeroPoint) / zeroPoint);
+  }
+
+  return chartDataVariation;
+}
+
+
+function prepareDataforCbh(star5: number[], star1: number[], fromIndex: number) {
+  const arrLength = star5.length;
+  let serie5star = calculateDeviations(star5, fromIndex),
+    serie1star = calculateDeviations(star1, fromIndex),
+    area5starTop = [],
+    area5starBottom = [],
+    area1starTop = [],
+    area1starBottom = [];
+
+  for (let i = 0, l = arrLength; i < l; i++) {
+    let item5star = serie5star[i],
+      item1star = serie1star[i];
+
+
+    let elTop5 = (item5star > 0) ? ((item5star > item1star) ? item5star : item5star) : 0
+    area5starTop.push(elTop5);
+
+    let elBot5 = (item5star > 0) ? ((item1star > 0) ? ((item1star > item5star) ? item5star : item1star) : 0) : item5star
+    area5starBottom.push(elBot5);
+
+    let elTop1 = (item1star > 0) ? item1star : ((item5star > 0) ? 0 : ((item5star < item1star) ? item1star : item5star));
+    area1starTop.push(elTop1);
+
+    let elBot1 = (item1star > 0) ? 0 : ((item5star < item1star) ? item1star : item1star);
+    area1starBottom.push(elBot1);
+  }
+  return { serie5star, area5starTop, area5starBottom, serie1star, area1starTop, area1starBottom };
+}
+
+//функция создает и настраивает Chart как на сайте
+function CbhChart(): Chart {
+
+  const chart = new Chart(document.getElementById('indexChart'), [0, 900], [0, 2000]);
+  chart.setCanvasPaddings(25, 50, 35, 20); // задаем отступы для области отрисовки
+
+  // ось X
+  chart.xAxis.setOptions(2, '#7F7F7F');
+  chart.xAxis.ticks.display = true;
+  chart.xAxis.ticks.settickDrawOptions(6, 2, '#7F7F7F');
+  chart.xAxis.ticks.label.setOptions('#B2B2B2', 'bottom', 11, ['11', '"Transcript Pro"']);
+
+  // ось Y
+  chart.yAxis.setOptions(2, '#B2B2B2', [1, 2]);
+  chart.yAxis.display = true;
+  chart.yAxis.position = 'end';
+  chart.yAxis.ticks.label.setOptions('#B2B2B2', 'right', 20, ['11', '"Transcript Pro"']);
+
+  // создаем Plots
+  chart.addPlot('red_line', 'line', 1, '#FF2222', '#FF2222');
+  chart.addPlot('red_area', 'area', 0, '#FFE5E5', '#FFE5E5', 0);
+  chart.addPlot('blue_line', 'line', 1, '#0070FF', '#0070FF', 1);
+  chart.addPlot('blue_area', 'area', 0, '#D9EAFF', '#D9EAFF', 0);
+  chart.addPlot('black_line', 'line', 1, '#000000', '#000000', 1);
+
+  // создаем Tooltipы
+
+  // lines
+  chart.findPlotById('red_line')?.addTooltip('ttId', 'line_vertical_full', 1, '#B2B2B2', [1, 2]);
+  chart.findPlotById('red_line')?.addTooltip('ttId', 'line_horizontal_end', 1, '#B2B2B2', [1, 2]);
+  chart.findPlotById('blue_line')?.addTooltip('ttId', 'line_horizontal_end', 1, '#B2B2B2', [1, 2]);
+
+  // circles
+  chart.findPlotById('blue_line')?.addTooltip('ttId', 'circle_series', 3, '#ffffff', '#0070FF', 4);
+  chart.findPlotById('red_line')?.addTooltip('ttId', 'circle_series', 3, '#ffffff', '#FF2222', 4);
+  chart.findPlotById('black_line')?.addTooltip('ttId', 'circle_series', 3, '#ffffff', 'black', 4);
+
+  chart.findPlotById('blue_line')?.addTooltip('ttId', 'circle_y_end', 3, '#ffffff', '#0070FF', 4);
+  chart.findPlotById('red_line')?.addTooltip('ttId', 'circle_y_end', 3, '#ffffff', '#FF2222', 4);
+
+  // labels
+  chart.findPlotById('red_line')?.addTooltip('ttId', 'label_x_start', 0.5, 'black', '#ebebeb', 4, xLabels).label.setOptions('black', 'bottom', 11, ['11', '"Transcript Pro"']);
+
+  // data
+  chart.findPlotById('red_line')?.addTooltip('ttId', 'data_y_end', 0.5, '#FF2222', '#FF2222', 4).label.setOptions('white', 'right', 11, ['11', '"Transcript Pro"']);
+  chart.findPlotById('blue_line')?.addTooltip('ttId', 'data_y_end', 0.5, '#0070FF', '#0070FF', 4).label.setOptions('white', 'right', 11, ['11', '"Transcript Pro"']);
+
+  // delta
+  chart.findPlotById('red_line')?.addTooltip('delta_1', 'delta_abs', 0.5, 'black', '#ebebeb', 4).label.setOptions('black', 'right', 20, ['11', '"Transcript Pro"']);
+  chart.findPlotById('blue_line')?.addTooltip('delta_1', 'delta_abs', 0.5, 'black', '#ebebeb', 4).label.setOptions('black', 'right', 20, ['11', '"Transcript Pro"']);
+
+  // подготавливаем данные как на сайте CyberHedge
+  let data = prepareDataforCbh(cbh5, cbh1, 0);
+  let {
+      serie5star,
+      area5starTop,
+      area5starBottom,
+      serie1star,
+      area1starTop,
+      area1starBottom
+  } = data;
+
+  // создаем Series
+  chart.addSeries('cyberHedge5_area', area5starTop, area5starBottom).setPlotsIds('blue_area');
+  chart.addSeries('cyberHedge1_area', area1starTop, area1starBottom).setPlotsIds('red_area');
+  chart.addSeries('cyberHedge5_line', serie5star).setPlotsIds('blue_line');
+  chart.addSeries('cyberHedge1_line', serie1star).setPlotsIds('red_line');
+  chart.addSeries('zero_line', zeroSeries).setPlotsIds('black_line');
+
+  //настраиваем параметры осей
+  chart.yAxis.ticks.setOptions('niceCbhStep', [1, 5, 10, 15, 20, 25, 30]);
+  chart.yAxis.ticks.label.units = '%';
+
+  chart.xAxis.ticks.setCustomLabels(xLabels);
+  chart.xAxis.ticks.setOptions('customDateTicks', ['half month', 'year', 'half year', 'third year', 'quarter year']);
+  chart.xAxis.display = true;
+
+  // настраиваем Min Max осей
+  chart.xAxis.setMinMax(chart.data.findExtremes('val'), true); //по экстремумам оси X
+  chart.yAxis.setMinMax(chart.data.findExtremes('ind', chart.xAxis.min, chart.xAxis.max), true); //scale to fit по Y
+  //chart.yAxis.setMinMax([chart.yAxis.min-0.05*chart.yAxis.length, chart.yAxis.max+0.05*chart.yAxis.length]); //добавляем по отступам как на сайте
+
+  //включаем анимацию
+  chart.xAxis.ticks.switchAnimation(true, 300);
+  chart.yAxis.ticks.switchAnimation(true, 300);
+  chart.switchDataAnimation(true, 300);
+
+  return chart;
+}
 
 
 
