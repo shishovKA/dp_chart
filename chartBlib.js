cf49f6c666f31dfd211d47be2d1cd3c9295c0247
Konstantin Shishov <shishov.k.a@yandex.ru>
2020-10-28 17:15:51 +0300

    solve ranges remember option bug 2

--- chartBlib.js
+++ chartBlib.js
@@ -2846,9 +2846,9 @@ WebFont.load({
                 cbh5 = chartData[1].slice(1).map(function (el) { return +el; });
                 xLabels = chartData[0].slice(1).map(function (el) { return new Date(el); });
                 zeroSeries = cbh1.map(function () { return 0; });
-                var max = xLabels.length - 1;
-                var min = 0;
-                reorganizeChart(cbh5, cbh1, min, max, false);
+                //const max = xLabels.length - 1;
+                //const min = 0;
+                //reorganizeChart(cbh5, cbh1, min, max, false);
                 // @ts-ignore
                 rangeSelected.click(rangeSelected);
             });
