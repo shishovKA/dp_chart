b4541a03d130394cc527f3c6a3699cb7911144cf
Konstantin Shishov <shishov.k.a@yandex.ru>
2020-11-10 14:33:07 +0300

    fixed color scheeme

--- src/classes/BackGround.ts
+++ src/classes/BackGround.ts
@@ -19,7 +19,22 @@ export class BackGround {
     }
 
     drawColoredGrid(ctx: CanvasRenderingContext2D, xCoord: Point[], yCoord: Point[]) {
+        ctx.globalAlpha = 0.1;
+        const colorPalette: string[] = ['#8CCB76', '#BED68D', '#E7D180', '#CC9263', '#CF5031'];
+
+        for (let i=0; i<xCoord.length-1; i++) {
+            ctx.fillStyle = colorPalette[i];
+            ctx.fillRect(xCoord[i].x, yCoord[0].y, xCoord[i+1].x - xCoord[i].x, yCoord[yCoord.length-1].y - yCoord[0].y );
+        }
+
+        for (let i=0; i<yCoord.length-1; i++) {
+            ctx.fillStyle = colorPalette[i];
+            ctx.fillRect(xCoord[0].x, yCoord[i].y, xCoord[xCoord.length-1].x - xCoord[0].x, yCoord[i+1].y - yCoord[i].y  );
+        }
+
+        ctx.globalAlpha = 1;
         
+        /*
         for (let i=1; i<xCoord.length; i++) {
             for (let j=1; j<yCoord.length; j++) {
                 let r = (246-233)/((xCoord.length-1)*(xCoord.length-1));
@@ -28,8 +43,8 @@ export class BackGround {
                 ctx.fillStyle = `rgb(${233+r*i*j}, ${246-g*i*j}, ${230-b*i*j})`
                 ctx.fillRect(xCoord[i].x, yCoord[j].y, xCoord[i-1].x - xCoord[i].x, yCoord[j-1].y - yCoord[j].y);
             }
-
         }
+        */
     }
 
 }
